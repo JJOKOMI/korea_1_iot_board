673619fff7d7ac24009de8bdc6558dcb7b9a7511
guswl458@gmail.com <guswl458@gmail.com>
2024-10-28 11:49:14 +0900

    front

---
+++ board_front/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const MAIN_PATH = '/';
+export const AUTH_PATH = '/auth';
+export const BOARD_DETAIL_PATH = (boardNumber: number | string) => `/board/detail/${boardNumber}`;
+export const BOARD_WRITE_PATH = '/board/write';
+export const BOARD_UPDATE_PATH = (boardNumber: number | string) => `/board/update/${boardNumber}`;
+export const USER_PATH = '/user';
+export const REACT_STUDY_PATH = '/react-study';
---
+++ board_front/src/react-study/기초.ts
@@ -0,0 +1,9 @@
+export const tmp= "";
+/*
+  - 터미널 열기: ctrl + shift + `
+  - 줄 복사: alt + shift + 방향키
+  - 줄 이동: alt + 방향키
+  - 내어쓰기: shift + tab
+  - 사이드바 토글: ctrl + b
+  - 검색: ctrl + f
+*/
